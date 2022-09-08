9026a55195ef6231f795b91e151270287e6efd2a
Aaryan Khan <aaryan19khan@gmail.com>
2022-09-08 10:02:26 -0500

    created new folders and did 1 assignment

---
+++ src/main/kotlin/week02/income.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var name = "Aaryan Khan"
+    var age = 23
+    var income = 100000.00
+    println("My name is $name, my age is $age and ")
+    println("I hope to earn $$income per year.")
+}
