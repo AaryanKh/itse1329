880a26429c3d2f5252a1e1e6be1a98cfae06c057
AaryanKhan <aaryan19khan@gmail.com>
2022-09-23 19:27:30 -0500

    Week 4 assignments

---
+++ src/main/kotlin/week04/SumOfNumbers.kt
@@ -0,0 +1,10 @@
+fun main(){
+    println("Please enter a positive non zero value >> ")
+    val userInput = Integer.valueOf(readLine())
+    var totalNumber = 0
+    for(i in 1 until  userInput+1){
+        totalNumber += i
+    }
+    println("The sum of numbers from 1 to $userInput is >> ")
+    println(totalNumber)
+}
